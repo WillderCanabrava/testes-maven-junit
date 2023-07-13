94b86ce411f7ed7a1eda264be49ea2a9def2b505
Willder Canabrava <willcanabrava@gmail.com>
2023-07-13 12:45:53 -0300

    ALterando número no método "subtrai".

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Calculadora {
+public class Calculadora {
+
+    private int a, b;
+
+    public Calculadora(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int soma() {
+        return this.a + this.b;
+    }
+
+    public int subtrai() {
+        return this.a - this.b;
+    }
+
+    public int multiplica() {
+        return this.a * this.b;
+    }
+
+    public int divide() {
+        return this.a / this.b;
+    }
+
 }
--- src/test/java/CalculadoraTest.java
+++ src/test/java/CalculadoraTest.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class CalculadoraTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculadoraTest {
+
+    @Test
+    public void testSoma () {
+        Calculadora calc = new Calculadora(7, 3);
+        Assert.assertEquals(10, calc.soma(), 0);
+    }
+
+    @Test
+    public void testSubtrai() {
+        Calculadora calc = new Calculadora(9, 4);
+        Assert.assertEquals(5, calc.subtrai(), 0);
+    }
+
+    @Test
+    public void testMultiplica() {
+        Calculadora calc = new Calculadora(18, 11);
+        Assert.assertEquals(198, calc.multiplica(), 0);
+    }
+
+    @Test
+    public void testDivide() {
+        Calculadora calc = new Calculadora(100, 4);
+        Assert.assertEquals(25, calc.divide(), 0);
+    }
 }
